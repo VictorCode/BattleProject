04e5d082c88cabd76cbf15c3cb25b9ec9e5eb622
THolovacko <tom.holovacko@gmail.com>
2015-07-01 21:52:03 -0400

    Head-Looking Power Animations
    IK for looking up and down and basic functionality for power animations
    are done. Need to look into networking and start setting up multiplayer
    functionality.

---
+++ Assets/Scripts/IKHead.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IKHead : MonoBehaviour 
+{
+	protected Animator animator;
+	public bool ikActive = false;
+	public Transform lookObj = null;
+		
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+		lookObj = GameObject.Find("Center").transform;
+		ikActive = true;
+	}
+	
+	void OnAnimatorIK()
+	{
+		if(animator) 
+		{ 
+			if(ikActive)
+			{
+				// Set the look target position, if one has been assigned
+				if(lookObj != null)
+				{
+					animator.SetLookAtWeight(1);
+					animator.SetLookAtPosition(lookObj.position);
+				}    
+			}
+		}
+		else
+		{
+			animator.SetLookAtWeight(0);
+		}
+	}
+}
